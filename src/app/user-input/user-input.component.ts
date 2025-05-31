11da277b1160115498216364a00fcc651b9c9ba1
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2025-05-30 22:23:50 -0400

    add user input component

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  initialInvestment = 0;
+  annualInvestment = 0;
+  expectedReturn = 0;
+  investmentDuration = 0;
 
+  onReset() {
+    console.log("resetting the form");
+    this.initialInvestment = 0;
+    this.annualInvestment = 0;
+    this.expectedReturn = 0;
+    this.investmentDuration = 0;
+  }
 }
+
